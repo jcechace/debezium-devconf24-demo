4bfe5a93945f8d9c7db89c34ccd35479efa8a4b6
Jakub Cechacek <jcechace@gmail.com>
2024-06-14 11:33:03 +0200

    Better structure of deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,25 +4,36 @@ read -p "Press key to create namespaces"
 kubectl create -f k8s/0001_namespaces.yml
 
 read -p "Press key to install Debezium Operator"
-helm repo add debezium https://charts.debezium.io
+helm repo add  --force-update debezium https://charts.debezium.io
 helm install debezium-operator debezium/debezium-operator --version 2.7.0-beta2 -n crypto-demo
 
+echo ""
 read -p "Press key to install Grafana Operator"
 helm install grafana-operator oci://ghcr.io/grafana/helm-charts/grafana-operator --version v5.6.3 -n crypto-infra
 
+echo ""
 read -p "Press key to deploy Legacy Crypto Application"
 kubectl -n crypto-legacy create -f k8s/crypto-legacy/0001_postgres.yml
 kubectl -n crypto-legacy create -f k8s/crypto-legacy/0002_crypto-app.yml
 
+echo ""
 read -p "Press key to deploy Redis and Monitoring infrastructure"
 kubectl -n crypto-infra create -f k8s/crypto-infra
 kubectl -n crypto-infra create -f k8s/crypto-infra/grafana
 
-read -p "Press key to deploy Knative components"
+echo ""
+read -p "Press key to deploy Knative broker"
 kubectl -n crypto-demo create -f k8s/crypto-demo/0001_broker.yml
+
+echo ""
+read -p "Press key to deploy CDC component"
 kubectl -n crypto-demo create -f k8s/crypto-demo/0002_crypto-cdc.yml
+
+echo ""
+read -p "Press key to deploy crypto-collector service"
 kubectl -n crypto-demo create -f k8s/crypto-demo/0003_crypto-collector.yml
 
+echo ""
 read -p "Press key to deploy Grafana Dashboards"
 kubectl -n crypto-infra create -f k8s/crypto-demo/dashboards/0001_dashboard-cdc.yaml
 kubectl -n crypto-infra create -f k8s/crypto-demo/dashboards/0002_dashboard-redis.yaml
